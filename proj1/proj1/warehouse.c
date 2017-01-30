34076cbe241c907c6ab31a3df2e2e47bc62d533a
cforno12 <cforno1@binghamton.edu>
2017-01-30 15:10:48 -0500

    First project of the semester. Description of the project is described in the .pdf in the proj1 folder

---
+++ proj1/proj1/warehouse.c
@@ -0,0 +1,17 @@
+//includes slots.h (an interface)
+#include "slots.h"
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+	int bin;
+	initSlots();
+	while(1==scanf("%d",&bin)) {
+		if (-1==findSlot(bin)) {
+			// getBin parameter should have a number between 0-3
+			getBin(bin,0);
+		}
+		getWidget(bin);
+	}
+	printEarnings();
+	return 0;
+}
