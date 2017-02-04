21b8fc8aa178c90802484b1e9f27ca85b60da58c
cforno12 <cforno1@binghamton.edu>
2017-02-04 12:46:47 -0500

    main updated to sort the workbench accordingly

--- proj1/proj1/warehouse.c
+++ proj1/proj1/warehouse.c
@@ -4,11 +4,23 @@
 
 int main(int argc, char ** argv) {
 	int bin;
+	int index = 0;
 	initSlots();
-	while(1==scanf("%d",&bin)) {
+	//as long as there are more bin numbers on the order for the kit....continunes onto the while loop
+	while(1 == scanf("%d",&bin)) {
 		if (-1==findSlot(bin)) {
-			// getBin parameter should have a number between 0-3
-			getBin(bin,0);
+			// getBin index should have a number between 0-3
+			if(bin < 0 || bin > 99){
+				//if customer orders something that we don't have.
+				return -1;
+			} else {
+				getBin(bin,index);
+				if(index < 3){
+					index++;
+				} else {
+					index = 0;
+				}
+			}
 		}
 		getWidget(bin);
 	}
