33ae3961977ac4934dfe62cb87f54cb8616f2f3e
cforno12 <cforno1@binghamton.edu>
2017-02-14 19:17:06 -0500

    lab04 completed

--- lab04/lab04/pointers.c
+++ lab04/lab04/pointers.c
@@ -10,43 +10,59 @@ void setBit(int *in,int ind,int val);
 
 int main(int argc,char **argv) {
 
-	printf("argc is at location %p\n",/* put correct expression here */);
+	printf("argc is at location %p\n",&argc);
 
 	int counter=0;
 
-	printf("counter variable is at location %p\n",/* put correct expression here */ );
+	printf("counter variable is at location %p\n",&counter);
 	printf("counter value is %d\n",counter);
 	/* Invoke the increment function to increment the value of counter here */
+	increment(&counter);
 	printf("counter value after increment is %d\n",counter);
 
 	int a=17;
 	int b=24;
 	printf("Before any swap, a=%d and b=%d\n",a,b);
+	swap_temp(&a, &b);
 	/* Invoke the swap_temp function to switch the values of a and b */
 	printf("After swap_temp, a=%d and b=%d\n",a,b);
+	swap_notemp(&a,&b);
 	/* Invoke the swap_notemp function to switch the values of a and b again */
 	printf("After swap_notemp, a=%d and b=%d\n",a,b);
 
 	int vec[6]={10,11,12,13,14,15};
 
 	printf("vec variable is at location %p\n",vec);
-	printf("vec vector index 0 is at %p\n",/* insert an expression with vec[0] */);
-	printf("vec vector index 1 is at %p\n",/* insert an expression with vec[1] */);
-	printf("vec vector index 5 is at %p\n",/* insert an expression with vec[5] */);
+	printf("vec vector index 0 is at %p\n",&vec[0]);
+	printf("vec vector index 1 is at %p\n",&vec[1]);
+	printf("vec vector index 5 is at %p\n",&vec[5]);
 	printf("Difference between location of vec[0] and vec[5] is %d\n",
-		(int)(/* insert an expression to subtract the location of vec[5] from the location of vec[0] */));
+		(int)((&vec[0]) - (&vec[5])/* insert an expression to subtract the location of vec[5] from the location of vec[0] */));
 	return 0;
 }
 
 /* Insert code for increment, swap_temp, and swap_notemp here */
 void increment(int *counter) {
-	counter++;
+	(*counter)++;
 }
 
 void swap_temp(int *a, int *b) {
-	int temp = a;
+	int temp = *a;
+	*a = *b;
 	*b = temp;
-	*a = b;
+}
+
+void swap_notemp(int *a, int *b) {
+	int i;
+	for (i = 0; i < 32; i++) {
+		if(getBit(a,i)){
+			setBit(a,i,getBit(b,i));
+			setBit(b,i,1);
+		} else {
+			setBit(a,i,getBit(b,i));
+			setBit(b,i,0);
+		}
+	}
 }
 
 int getBit(int *in,int ind) {
