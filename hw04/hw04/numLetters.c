93198a91ef5171ad4eb417fdd4f0a239bbcf60e4
cforno12 <cforno1@binghamton.edu>
2017-02-15 17:12:31 -0500

    worked on hw04

--- hw04/hw04/numLetters.c
+++ hw04/hw04/numLetters.c
@@ -5,52 +5,43 @@ int main(int argc, char ** argv) {
 	The program need to calculate the number of letters required to write the
 	number using English words.
 	*/
-	int num;
 
-	scanf("%i\n", &num);
-
+	int num = atoi(argv[1]);
+	//if the number is inputted is less than zero or greater than a billion
+	//the program is not equipped to handle those types of numers
 	if(num < 0 || num > 1000000000){
 		return 1;
 	}
-	int bill = 0;
-	int hundMill = 0;
-	int tensMill = 0;
-	int onesMill = 0;
-	int hundThou = 0;
-	int tensThou = 0;
-	int onesThou = 0;
-	int hund = 0;
-	int tens = 0;
-	int ones = 0;
-
-	int numArray[] = {bill, hundMill, tensMill, onesMill, hundThou, tensThou, onesThou, hund, tens, ones};
-	int temp = 0;
-	while (num > 0) {
-		if(num == 1000000000){
-			temp = num;
-			temp /= 1000000000;
-			numArray[0] = temp;
-			num -= temp * 1000000000;
-			continue;
+	//numArray is an array which contains all the digits in the inputted number
+	//each in its own index.
+	int numArray[10];
+	int i;
+	int size = 0; //the size of the numArray.
+	for (i = 0; i < 10; i++) {
+		if(num != 0){
+			numArray[i] = num % 10;
+		 	num /= 10;
+		} else {
+			size = i
+			break;
 		}
-		if(num > 100000000){
-			temp = num;
-			temp /= 100000000;
-			numArray[1] = temp;
-			num -= temp * 100000000;
-			continue;
-		}
-		if(num > 10000000){
-			temp = num;
-			temp /= 10000000;
-			numArray[2] = temp;
-			num -= temp * 10000000;
-			continue;
-		}
-		if(num > )
 	}
-	int result = 0;
+	// words after 20 is just repeated
+	int baseNum[] = {4 ,3 ,3 ,5 ,4 ,4 ,3 ,5 ,5 ,4 ,3 ,6 ,6 ,8 ,8 ,7 ,7 ,9 ,8 ,7 ,6};
+	//size 21
+	//starts with 30
+	int tensNum[] = {6 ,6 ,5 ,5 ,7 ,6 ,5};
+	//size 7
+	//hundred, thousand, million, billion
+	int commaNum[] = {7, 8, 7, 7};
+	//size 4
+	int retVal = 0;
+	if(num == 1000000000 ){
+		retVal += commaNum[3];
+	}
+	if()
 
-	printf("%d takes %d letters\n", num, result);
+
+	//printf("%d takes %d letters\n", num, result);
 	return 0;
 }
