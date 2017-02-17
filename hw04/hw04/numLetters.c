e942aee4f3a02779411c1d51e9fb564d13cf3b28
cforno12 <cforno1@binghamton.edu>
2017-02-16 23:16:11 -0500

    put function declaration on top

--- hw04/hw04/numLetters.c
+++ hw04/hw04/numLetters.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void over100(int* num);
 int retVal = 0;
 int baseNum[] = {4 ,3 ,3 ,5 ,4 ,4 ,3 ,5 ,5 ,4 ,3 ,6 ,6 ,8 ,8 ,7 ,7 ,9 ,8 ,8};
 int tensNum[] = {6, 6 ,5 ,5 ,5 ,7 ,6 ,6};
