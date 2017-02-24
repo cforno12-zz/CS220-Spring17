78ed782b94c044add44ecddf21f0b64b7dd0ce6e
cforno12 <cforno1@binghamton.edu>
2017-02-24 13:11:57 -0500

    started to define the functions within arrayList.c

--- hw05/hw05/arrayList.c
+++ hw05/hw05/arrayList.c
@@ -25,6 +25,49 @@ bool arrayListEnlarge(arrayList list) {
 
 /* Put definitions of the other arrayList methods in arrayList.h here */
 
+bool arrayListAdd(arrayList list, int item){
+	assert(list);
+	//increment numUsed by 1
+	//
+}
+void arrayListClear(arrayList list){
+	assert(list);
+	list->numUsed = 0;
+}
+bool arrayListContains(arrayList list, int item){
+	assert(list);
+	int retVal = false;
+	if(*list->data == item){
+		retVal = true;
+	} else if(list->data == NULL){
+		retVal = false;
+	} else {
+		arrayListContains(list->data, item);
+	}
+}
+int arrayListGet(arrayList list,int index){
+	assert(list);
+}
+int arrayListIndexOf(arrayList list, int item){
+	assert(list);
+	//should return a -1 if it cannot find its argument in the arrayList,
+	//just like the Java "indexOf" method.
+}
+bool arrayListIsEmpty(arrayList list){
+	assert(list);
+}
+int arrayListSet(arrayList list, int index, int item){
+	assert(list);
+}
+int arrayListSize(arrayList list){
+
+}
+char * arrayListToString(arrayList list,char *buffer){
+	assert(list);
+	//should be written into the space provided by your caller in the
+	//"buffer" argument, and you should return the buffer when you are finished
+}
+
 void arrayListFree(arrayList list) {
 	free(list->data);
 	free(list);
