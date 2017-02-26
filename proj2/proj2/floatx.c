c4fb4755ce8f3c8abd4fce87d9db54c8eff46dc1
cforno12 <cforno1@binghamton.edu>
2017-02-26 01:21:59 -0500

    added proj2

---
+++ proj2/proj2/floatx.c
@@ -0,0 +1,18 @@
+#include "floatx.h"
+
+/*--------------------------------------------------------------------------------
+	Return floatx representation (as defined by *def) which
+	best approximates value
+-------------------------------------------------------------------------------- */
+floatx doubleToFloatx(const floatxDef *def, double value) {
+	/* Put your code here */
+	return 0;
+}
+
+/** Return C double with value which best approximates that of floatx fx
+ *  (as defined by *def).
+ */
+double floatxToDouble(const floatxDef *def, floatx fx) {
+	/* Put your code here */
+	return NAN;
+}
