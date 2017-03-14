ce43a1f7aa08d8902afbe3e344187e51f8543572
cforno12 <cforno1@binghamton.edu>
2017-03-14 01:00:13 -0400

    turned the double value to floatx

--- proj2/proj2/floatx.c
+++ proj2/proj2/floatx.c
@@ -1,23 +1,54 @@
 #include "floatx.h"
 
 /*--------------------------------------------------------------------------------
-  Return floatx representation (as defined by *def) which
-  best approximates value
-  -------------------------------------------------------------------------------- */
+	Return floatx representation (as defined by *def) which
+	best approximates value
+-------------------------------------------------------------------------------- */
+
+int pow_of_two(int exponent);
+
 floatx doubleToFloatx(const floatxDef *def, double value) {
-    floatx retVal = (floatx) malloc(sizeof(floatx));
-    if(def->totBits == sizeof(value)){
-        
-    }
+
+	floatx bits = *((floatx*)&value);
+	int fracBits = def->totBits - def->expBits - 1;
+	floatx retVal = 0;
+
+	int biased = pow_of_two((def->expBits) - 1);
+	bits = (bits - 1023) + biased - 1;
+
+	// I started with making a helper function to get a range of bits from a number.
+	// You need to take those and convert them into smaller bits.
+	// The fraction is the rest of the bits and you can just throw out the ones at the end you don't need.
+	// Use some bit shifting for that.
+	// You have to look at the individual bits of the value, the first bit is the sign bit, the next 11 are the exponent and the next 52 are the fraction.
+	// You can just truncate the fraction to however many bits the definition calls for but you have to convert the exponent by subtracting 1023 and then adding 2^(expBits-1)-1 to it.
+	return retVal;
 }
 
-/** Return C double with value which best approximates that of floatx fx
- *  (as defined by *def).
+/*
+ * Return C double with value which best approximates that of floatx fx
+ * (as defined by *def).
  */
 double floatxToDouble(const floatxDef *def, floatx fx) {
+
+	double bits = *((double*)&fx);
+	int fractBits = def->totBits - def->expBits - 1;
 	double retVal = 0.0;
 
-    
+	
 
 	return retVal;
+}
+
+int pow_of_two(int exponent){
+	int retVal = 1;
+	if(exponent == 0){
+		return 1;
+	} else if (exponent == 1){
+		return 2;
+	}
+	int i;
+	for (i = 0; i < exponent; i++) {
+		retVal *= 2;
+	}
 }
