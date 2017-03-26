a2157833b6c396f307f44eb121536abbc99d65f6
Cristobal Forno <cforno1@binghamton.edu>
2017-03-25 21:47:30 -0400

    modified test.c to test the questions in hw06.txt

--- hw06/test.c
+++ hw06/test.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main(int argc, char ** argv){
-    int x = -5;
-    int y = (x | (-1-x));
-
-    printf("Size of int is: %lu\n", sizeof(y));
-
-    if (y == -1){
-        printf("How did you know that?\n");
-    } else {
-        printf("Special value of x\n!");
-    }
+    float big=1e20;
+    float little=-1e20;
+    float pi=3.14;
+    float y = (big + little)+pi;
+    float z = big + (little + pi);
+    float x =  (little + pi);
+    printf("y == %f\n",y);
+    printf("(little + pi) == %f\n", x);
+    printf("little == %f", little);
+    printf("z == %f\n",z);
+    if (y==z) printf("This is an example of the associative property.\n");
+    else printf("This is a counter-example.. the associative property does not hold!\n");
 }
